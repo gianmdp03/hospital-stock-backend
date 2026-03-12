42d83f9e3ea838a4118567f003f35b3c9cfdc62e
Joaquin <joaco.rodriguezortega@gmail.com>
2026-03-12 18:10:51 -0300

    More files

--- src/main/java/com/gjd/hospital_stock_backend/repository/CategoryRepository.java
+++ src/main/java/com/gjd/hospital_stock_backend/repository/CategoryRepository.java
@@ -1,4 +1,11 @@
 package com.gjd.hospital_stock_backend.repository;
 
+import com.gjd.hospital_stock_backend.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface CategoryRepository {
+    Page<Category> findAllByNameContainingIgnoreCase(String name, Pageable pageable);
 }
--- src/main/java/com/gjd/hospital_stock_backend/service/CategoryService.java
+++ src/main/java/com/gjd/hospital_stock_backend/service/CategoryService.java
@@ -1,4 +1,19 @@
 package com.gjd.hospital_stock_backend.service;
 
+import com.gjd.hospital_stock_backend.dto.category.CategoryDetailDTO;
+import com.gjd.hospital_stock_backend.dto.category.CategoryListDTO;
+import com.gjd.hospital_stock_backend.dto.category.CategoryRequestDTO;
+import com.gjd.hospital_stock_backend.dto.category.CategoryUpdateDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface CategoryService {
+    CategoryDetailDTO addCategory(CategoryRequestDTO dto);
+    CategoryDetailDTO updateCategory(Long id, CategoryUpdateDTO dto);
+    Page<CategoryListDTO> listCategories(Pageable pageable);
+    CategoryDetailDTO getCategoryById(Long id);
+    Page<CategoryListDTO> searchCategories(String name, Pageable pageable);
+    void deleteCategory(Long categoryId);
 }
