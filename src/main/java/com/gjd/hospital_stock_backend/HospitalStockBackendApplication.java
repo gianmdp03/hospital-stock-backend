a34fcadc137854addb72b872c48d3281b73ff46b
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-03-12 17:02:24 -0300

    First Commit

---
+++ src/main/java/com/gjd/hospital_stock_backend/HospitalStockBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gjd.hospital_stock_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalStockBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalStockBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gjd/hospital_stock_backend/HospitalStockBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjd.hospital_stock_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalStockBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
