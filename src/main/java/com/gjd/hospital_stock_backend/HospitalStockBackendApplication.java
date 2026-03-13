1203c258ea7138e9944ea64ab0ca4ad4ce5f43ce
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-03-13 10:29:49 -0300

    Update HospitalStockBackendApplication.java

--- src/main/java/com/gjd/hospital_stock_backend/HospitalStockBackendApplication.java
+++ src/main/java/com/gjd/hospital_stock_backend/HospitalStockBackendApplication.java
@@ -2,11 +2,13 @@ package com.gjd.hospital_stock_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @SpringBootApplication
 public class HospitalStockBackendApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(HospitalStockBackendApplication.class, args);
 	}
 
