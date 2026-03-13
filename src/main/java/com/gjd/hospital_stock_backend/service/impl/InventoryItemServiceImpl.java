65f70cf2928c67b97be05c8fc7752faa2797c8c0
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2026-03-13 15:59:02 -0300

    add inventoryItem

--- src/main/java/com/gjd/hospital_stock_backend/service/impl/InventoryItemServiceImpl.java
+++ src/main/java/com/gjd/hospital_stock_backend/service/impl/InventoryItemServiceImpl.java
@@ -1,9 +1,16 @@
 package com.gjd.hospital_stock_backend.service.Impl;
 
+import com.gjd.hospital_stock_backend.dto.inventoryItem.InventoryItemDetailDTO;
+import com.gjd.hospital_stock_backend.dto.inventoryItem.InventoryItemRequestDTO;
+import com.gjd.hospital_stock_backend.exception.NotFoundException;
 import com.gjd.hospital_stock_backend.mapper.InventoryItemMapper;
+import com.gjd.hospital_stock_backend.model.InventoryItem;
+import com.gjd.hospital_stock_backend.model.Product;
 import com.gjd.hospital_stock_backend.repository.InventoryItemRepository;
+import com.gjd.hospital_stock_backend.repository.ProductRepository;
 import com.gjd.hospital_stock_backend.service.InventoryItemService;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.NotFound;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,4 +20,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class InventoryItemServiceImpl implements InventoryItemService {
     private final InventoryItemRepository inventoryItemRepository;
     private final InventoryItemMapper inventoryItemMapper;
+    private final ProductRepository productRepository;
+
+    @Override
+    public InventoryItemDetailDTO addInventoryItem(InventoryItemRequestDTO dto) {
+        Product product = productRepository.findByIdAndDisabledTrue(dto.productId())
+                .orElseThrow(()-> new NotFoundException("Product ID does not exist"));
+        InventoryItem inventoryItem = inventoryItemMapper.toEntity(dto);
+        inventoryItem.setProduct(product);
+        inventoryItemRepository.save(inventoryItem);
+        return inventoryItemMapper.toDetailDTO(inventoryItem);
+
+    }
+    
 }
