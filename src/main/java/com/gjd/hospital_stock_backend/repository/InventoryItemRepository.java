6adb03d9818b3cd3ebc8dc69b5501544c6e375d8
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2026-03-13 16:08:17 -0300

    getInventoryItemById

--- src/main/java/com/gjd/hospital_stock_backend/repository/InventoryItemRepository.java
+++ src/main/java/com/gjd/hospital_stock_backend/repository/InventoryItemRepository.java
@@ -1,9 +1,20 @@
 package com.gjd.hospital_stock_backend.repository;
 
 import com.gjd.hospital_stock_backend.model.InventoryItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InventoryItemRepository extends JpaRepository<InventoryItem, Long> {
+    Page<InventoryItem> findAllByProductId(Long productId, Pageable pageable);
+    List<InventoryItem> findByProductIdAndStockGreaterThanOrderByExpireDateAsc(Long productId, int stock);
+
+    List<InventoryItem> findTop10ByOrderByStockDesc();
+
+    List<InventoryItem> findByStockLessThan(int stockLimit);
+    Page<InventoryItem> findByProduct_NameContainingIgnoreCase(String name, Pageable pageable);
 }
