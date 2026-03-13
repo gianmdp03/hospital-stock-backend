eebca198b66ba445e6e324dd8ec454be01e7a77d
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2026-03-13 15:58:55 -0300

    productRepository method

--- src/main/java/com/gjd/hospital_stock_backend/repository/ProductRepository.java
+++ src/main/java/com/gjd/hospital_stock_backend/repository/ProductRepository.java
@@ -4,7 +4,9 @@ import com.gjd.hospital_stock_backend.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
+    Optional<Product>  findByIdAndDisabledTrue(Long id );
+    }
