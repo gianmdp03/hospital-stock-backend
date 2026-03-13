ed019f18cbb545b782f65ef9337cf8a07d233d26
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2026-03-13 15:58:43 -0300

    inventoryItem mapper

--- src/main/java/com/gjd/hospital_stock_backend/mapper/InventoryItemMapper.java
+++ src/main/java/com/gjd/hospital_stock_backend/mapper/InventoryItemMapper.java
@@ -1,7 +1,20 @@
 package com.gjd.hospital_stock_backend.mapper;
 
-import org.mapstruct.Mapper;
+import com.gjd.hospital_stock_backend.dto.inventoryItem.InventoryItemDetailDTO;
+import com.gjd.hospital_stock_backend.dto.inventoryItem.InventoryItemRequestDTO;
+import com.gjd.hospital_stock_backend.dto.inventoryItem.InventoryItemUpdateDTO;
+import com.gjd.hospital_stock_backend.model.InventoryItem;
+import org.mapstruct.*;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {ProductMapper.class})
 public abstract class InventoryItemMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "product", ignore = true)
+    public abstract InventoryItem toEntity(InventoryItemRequestDTO dto);
+    public abstract InventoryItemDetailDTO toDetailDTO(InventoryItem entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "product", ignore = true)
+    public abstract void updateEntityFromDto(InventoryItemUpdateDTO dto, @MappingTarget InventoryItem entity);
 }
