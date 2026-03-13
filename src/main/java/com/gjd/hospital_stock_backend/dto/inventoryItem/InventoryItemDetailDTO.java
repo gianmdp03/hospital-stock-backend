2218a4ca8f5906a61d2c34c121f29dc2bac9f81e
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2026-03-12 23:39:57 -0300

    inventoryItem Dto's

--- src/main/java/com/gjd/hospital_stock_backend/dto/inventoryItem/InventoryItemDetailDTO.java
+++ src/main/java/com/gjd/hospital_stock_backend/dto/inventoryItem/InventoryItemDetailDTO.java
@@ -1,5 +1,7 @@
 package com.gjd.hospital_stock_backend.dto.inventoryItem;
 
+import com.gjd.hospital_stock_backend.dto.product.ProductListDTO;
+
 import java.time.LocalDate;
 
 public record InventoryItemDetailDTO(Long id, ProductListDTO productListDTO, int stock, LocalDate expireDate) {
--- src/main/java/com/gjd/hospital_stock_backend/service/InventoryItemService.java
+++ src/main/java/com/gjd/hospital_stock_backend/service/InventoryItemService.java
@@ -1,4 +1,23 @@
 package com.gjd.hospital_stock_backend.service;
 
+import com.gjd.hospital_stock_backend.dto.inventoryItem.InventoryItemDetailDTO;
+import com.gjd.hospital_stock_backend.dto.inventoryItem.InventoryItemRequestDTO;
+import com.gjd.hospital_stock_backend.dto.inventoryItem.InventoryItemUpdateDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface InventoryItemService {
+
+    InventoryItemDetailDTO addInventoryItem(InventoryItemRequestDTO dto);
+    InventoryItemDetailDTO updateInventoryItem(Long id, InventoryItemUpdateDTO dto);
+    Page<InventoryItemDetailDTO> listInventoryItems(Pageable pageable);
+    InventoryItemDetailDTO getInventoryItemById(Long id);
+    void deleteInventoryItem(Long inventoryItemId);
+
+    List<InventoryItemDetailDTO> getTopStockItems();
+    List<InventoryItemDetailDTO> getLowStockItems(int stockLimit);
+
+    Page<InventoryItemDetailDTO> searchByProductName(String name, Pageable pageable);
 }
