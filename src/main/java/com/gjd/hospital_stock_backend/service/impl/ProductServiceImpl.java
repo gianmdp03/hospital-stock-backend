a41a77bb36b8531145b0bf31da0e69eb913ff998
Joaquin <joaco.rodriguezortega@gmail.com>
2026-03-16 15:25:18 -0300

    Add ProductServiceImpl methods

--- src/main/java/com/gjd/hospital_stock_backend/service/impl/ProductServiceImpl.java
+++ src/main/java/com/gjd/hospital_stock_backend/service/impl/ProductServiceImpl.java
@@ -1,12 +1,26 @@
-package com.gjd.hospital_stock_backend.service.Impl;
+package com.gjd.hospital_stock_backend.service.impl;
 
 
+import com.gjd.hospital_stock_backend.dto.product.ProductDetailDTO;
+import com.gjd.hospital_stock_backend.dto.product.ProductListDTO;
+import com.gjd.hospital_stock_backend.dto.product.ProductRequestDTO;
+import com.gjd.hospital_stock_backend.dto.product.ProductUpdateDTO;
+import com.gjd.hospital_stock_backend.exception.NotFoundException;
 import com.gjd.hospital_stock_backend.mapper.ProductMapper;
+import com.gjd.hospital_stock_backend.model.Category;
+import com.gjd.hospital_stock_backend.model.Product;
+import com.gjd.hospital_stock_backend.repository.CategoryRepository;
 import com.gjd.hospital_stock_backend.repository.ProductRepository;
 import com.gjd.hospital_stock_backend.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 @Transactional(readOnly = true)
@@ -14,4 +28,70 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    @Transactional
+    public ProductDetailDTO addProduct(ProductRequestDTO dto) {
+        Optional<Product> optionalProduct = productRepository.findByName(dto.name());
+        Set<Category> categories = (Set<Category>) categoryRepository.findAllById(dto.categoriesId());
+        if(categories.isEmpty())
+            throw new NotFoundException("List is empty");
+        if(optionalProduct.isPresent()){
+            Product existingProduct = optionalProduct.get();
+            existingProduct.setDisabled(true);
+            existingProduct.setCategories(categories);
+            existingProduct = productRepository.save(existingProduct);
+            return productMapper.toDetailDto(existingProduct);
+        }
+        Product product = productMapper.toEntity(dto);
+        product.setCategories(categories);
+        product = productRepository.save(product);
+        return productMapper.toDetailDto(product);
+    }
+
+    @Override
+    @Transactional
+    public ProductDetailDTO updateProduct(Long id, ProductUpdateDTO dto) {
+        Product product = productRepository.findByIdAndDisabledTrue(id).orElseThrow(()-> new NotFoundException("Product id does not exist"));
+        productMapper.updateEntityFromDto(dto,product);
+        if(!dto.categoriesId().isEmpty()){
+            Set<Category> categories = (Set<Category>) categoryRepository.findAllById(dto.categoriesId());
+            product.setCategories(categories);
+        }
+        product = productRepository.save(product);
+        return productMapper.toDetailDto(product);
+    }
+
+    @Override
+    public Page<ProductListDTO> listProducts(Pageable pageable) {
+        Page<Product> page = productRepository.findByDisabledTrue(pageable);
+        if (page.isEmpty())
+            throw new NotFoundException("List is empty");
+        return page.map(productMapper::toListDto);
+    }
+
+    @Override
+    public Page<ProductListDTO> searchProductsByName(String name, Pageable pageable){
+        if(name == null || name.isBlank()){
+            return Page.empty();
+        }
+        Page<Product> page = productRepository.findByNameContainingIgnoreCaseAndDisabledTrue(name, pageable);
+        return page.map(productMapper::toListDto);
+    }
+
+    @Override
+    public ProductDetailDTO getProductById(Long id){
+        Product product = productRepository.findByIdAndDisabledTrue(id)
+                .orElseThrow(()-> new NotFoundException("Product ID does not exist"));
+        return productMapper.toDetailDto(product);
+    }
+
+    @Override
+    @Transactional
+    public void deleteProduct(Long productId) {
+        Product product = productRepository.findByIdAndDisabledTrue(productId).orElseThrow(()->new NotFoundException("Product ID does not exist"));
+        product.setDisabled(false);
+        productRepository.save(product);
+    }
 }
