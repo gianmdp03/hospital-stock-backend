966e233b9de398a9f80ac3a65f0aac6fad9c6ed6
Joaquin <joaco.rodriguezortega@gmail.com>
2026-03-16 15:25:54 -0300

    Update ProductMapper entity

--- src/main/java/com/gjd/hospital_stock_backend/mapper/ProductMapper.java
+++ src/main/java/com/gjd/hospital_stock_backend/mapper/ProductMapper.java
@@ -1,7 +1,34 @@
 package com.gjd.hospital_stock_backend.mapper;
 
-import org.mapstruct.Mapper;
+import com.gjd.hospital_stock_backend.dto.product.ProductDetailDTO;
+import com.gjd.hospital_stock_backend.dto.product.ProductListDTO;
+import com.gjd.hospital_stock_backend.dto.product.ProductRequestDTO;
+import com.gjd.hospital_stock_backend.dto.product.ProductUpdateDTO;
+import com.gjd.hospital_stock_backend.model.Product;
+import org.hibernate.validator.internal.util.stereotypes.Lazy;
+import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
 public abstract class ProductMapper {
+    @Autowired
+    @Lazy
+    private InventoryItemMapper inventoryItemMapper;
+
+    @Autowired
+    @Lazy
+    private CategoryMapper categoryMapper;
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "inventoryItems", ignore = true)
+    @Mapping(target = "categories", ignore = true)
+    public abstract Product toEntity(ProductRequestDTO dto);
+    public abstract ProductDetailDTO toDetailDto(Product entity);
+    public abstract ProductListDTO toListDto(Product entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "inventoryItems", ignore = true)
+    @Mapping(target = "categories", ignore = true)
+    public abstract void updateEntityFromDto(ProductUpdateDTO dto, @MappingTarget Product product);
 }
