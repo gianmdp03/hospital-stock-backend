dc5faae6c7b74386eaa5262839a59feda47b80fb
Joaquin <joaco.rodriguezortega@gmail.com>
2026-03-16 15:25:04 -0300

    Add ProductService methods

--- src/main/java/com/gjd/hospital_stock_backend/service/ProductService.java
+++ src/main/java/com/gjd/hospital_stock_backend/service/ProductService.java
@@ -1,4 +1,17 @@
 package com.gjd.hospital_stock_backend.service;
 
+import com.gjd.hospital_stock_backend.dto.product.ProductDetailDTO;
+import com.gjd.hospital_stock_backend.dto.product.ProductListDTO;
+import com.gjd.hospital_stock_backend.dto.product.ProductRequestDTO;
+import com.gjd.hospital_stock_backend.dto.product.ProductUpdateDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ProductService {
+    ProductDetailDTO addProduct(ProductRequestDTO dto);
+    Page<ProductListDTO> listProducts(Pageable pageable);
+    ProductDetailDTO updateProduct(Long id, ProductUpdateDTO dto);
+    Page<ProductListDTO> searchProductsByName(String name, Pageable pageable);
+    ProductDetailDTO getProductById(Long id);
+    void deleteProduct(Long productId);
 }
