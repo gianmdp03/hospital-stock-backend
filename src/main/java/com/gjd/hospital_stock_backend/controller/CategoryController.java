582e3a946937045aaf0d5f9298ffb5d7ca0ccbf3
Joaquin <joaco.rodriguezortega@gmail.com>
2026-03-16 15:59:32 -0300

    Add CategoryController methods

--- src/main/java/com/gjd/hospital_stock_backend/controller/CategoryController.java
+++ src/main/java/com/gjd/hospital_stock_backend/controller/CategoryController.java
@@ -1,13 +1,58 @@
 package com.gjd.hospital_stock_backend.controller;
 
+import com.gjd.hospital_stock_backend.dto.category.CategoryDetailDTO;
+import com.gjd.hospital_stock_backend.dto.category.CategoryListDTO;
+import com.gjd.hospital_stock_backend.dto.category.CategoryRequestDTO;
+import com.gjd.hospital_stock_backend.dto.category.CategoryUpdateDTO;
 import com.gjd.hospital_stock_backend.service.CategoryService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/categories")
 @RequiredArgsConstructor
 public class CategoryController {
     private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<CategoryDetailDTO> addCategory(@Valid @RequestBody CategoryRequestDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.addCategory(dto));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<CategoryDetailDTO> updateCategory(@PathVariable Long id, @Valid @RequestBody CategoryUpdateDTO dto)
+    {
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.updateCategory(id, dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<CategoryListDTO>> listCategories(
+            @PageableDefault(page = 0, size = 10, sort = "name", direction = Sort.Direction.DESC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.listCategories(pageable));
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<CategoryDetailDTO> getCategoryById(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.getCategoryById(id));
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<Page<CategoryListDTO>> searchCategories(
+            @PathVariable String name,
+            @PageableDefault(page = 0, size = 10, sort = "name", direction = Sort.Direction.DESC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(categoryService.searchCategories(name, pageable));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long id){
+        categoryService.deleteCategory(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
